e00014a2784c0dad54ae5b7f07214d39a88489ea
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2020-12-22 12:53:34 +0100

    Bytt til gradle

---
+++ build.gradle.kts
@@ -0,0 +1,69 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.3.5.RELEASE"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    kotlin("jvm") version "1.4.21"
+    kotlin("plugin.spring") version "1.4.21"
+}
+
+group = "no.nav.helse.flex"
+version = "0.0.1-SNAPSHOT"
+description = "flex-reisetilskudd-gsak"
+java.sourceCompatibility = JavaVersion.VERSION_14
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.github.com/navikt/maven-release")
+    }
+
+    maven {
+        url = uri("https://maven.pkg.github.com/navikt/freg-security")
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.flywaydb:flyway-core")
+    implementation("no.nav.security:token-client-spring:1.3.1")
+    implementation("no.nav.security:token-validation-spring:1.3.1")
+    implementation("org.slf4j:slf4j-api")
+    implementation("org.aspectj:aspectjrt")
+    implementation("org.aspectj:aspectjweaver")
+    implementation("org.hibernate.validator:hibernate-validator")
+    implementation("org.springframework.boot:spring-boot-starter-logging")
+    implementation("net.logstash.logback:logstash-logback-encoder:4.10")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.retry:spring-retry")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
+    testImplementation("com.h2database:h2")
+    testImplementation("org.awaitility:awaitility")
+    testImplementation("org.hamcrest:hamcrest-library")
+    testImplementation("org.scala-lang:scala-library:2.12.11")
+
+}
+
+tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    this.archiveFileName.set("app.jar")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "14"
+    }
+}
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
